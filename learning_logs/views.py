de8819f215a364e05d874027c96b7c04ec67cf14
James <jamespairepo@gmail.com>
2024-03-26 21:04:04 -0700

    Update views.py

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TropicForm
+from .forms import TropicForm, EntryFrom
 
 
 def index(request):
@@ -38,3 +38,23 @@ def new_topic(request):
     # display a blank or invalid form
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """add a new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # no data submitted, create a blank form
+        form = EntryForm()
+    else:
+        # POST data submitted, process new data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+        
+    # display a blank or invalid form
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
