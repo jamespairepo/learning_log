2d7272978fc038d75b76d86e0ccdd725f3f0eb5e
James <jamespairepo@gmail.com>
2024-03-26 21:17:58 -0700

    Update views.py

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -26,7 +26,7 @@ def topic(request, topic_id):
 
 def new_topic(request):
     """add new topic"""
-    if request.method != 'POST':
+    if request.method != "POST":
         # no data submitted, create a blank form
         form = TopicForm()
     else:
@@ -34,18 +34,18 @@ def new_topic(request):
         form = TopicForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('learning_logs:topics')
+            return redirect("learning_logs:topics")
 
     # display a blank or invalid form
-    context = {'form': form}
-    return render(request, 'learning_logs/new_topic.html', context)
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
 
 
 def new_entry(request, topic_id):
     """add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
 
-    if request.method != 'POST':
+    if request.method != "POST":
         # no data submitted, create a blank form
         form = EntryForm()
     else:
@@ -55,8 +55,8 @@ def new_entry(request, topic_id):
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
-            return redirect('learning_logs:topic', topic_id=topic_id)
+            return redirect("learning_logs:topic", topic_id=topic_id)
 
     # display a blank or invalid form
-    context = {'topic': topic, 'form': form}
-    return render(request, 'learning_logs/new_entry.html', context)
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
