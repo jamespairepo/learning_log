b916cb8387e7b41aaecc724ea91fc0e7e2201f09
James <jamespairepo@gmail.com>
2024-03-26 21:28:36 -0700

    Update views.py

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic, Entry
-from .forms import TropicForm, EntryFrom
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
