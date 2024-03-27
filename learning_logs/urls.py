08f8ab40453a60fbf63e1d479828e6b0d81ff66a
James <jamespairepo@gmail.com>
2024-03-26 20:41:50 -0700

    Update urls.py

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("topics/", views.topics, name="topics"),
     # detail page for single topic
     path("topics/<int:topic_id>/", views.topic, name="topic"),
+    # page for adding a new topic
+    path('new_topic/', view.new_topic, name='new_topic'),
 ]
